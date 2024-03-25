97d9bdc7d87f1933f7139a7540ac26fd37998e24
Christian Flack <cflack@users.noreply.github.com>
2024-03-25 21:03:00 +0100

    fix: adds missing quotation marks

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "blog_sg" {
   subnet_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = [0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
-  egress_cidr_blocks  = [0.0.0.0/0]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
